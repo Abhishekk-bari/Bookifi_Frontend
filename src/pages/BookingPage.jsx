833d39d792c84d0f9851b72f34cffc9e35500724
Abhi <abhi2bari929@gmail.com>
2024-11-17 19:13:38 +0530

    fonts with booking page

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 function BookingPage() {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-    <h1>Book an Appointment</h1>
+    <div className='flex flex-col  justify-center w-screen h-screen'>
+    <div className="text_container pl-10 ">
+    <h1 className="text-8xl pb-10 font-['SummerCharming'] text-blue-500">Book</h1>
+    <p className="font-['CHRISTMASCOMEBACK'] text-6xl pb-2">Get a free appointment </p>
+    <p className="text-lg">Booking your photography session is just a few clicks away!</p>
+
+    </div>
     </div>
   )
 }
